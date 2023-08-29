8fa6bb496ac8d14cc2b3c4807326606d551fa36b
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-08-29 17:07:48 -0500

    Creandio el formulario del login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,26 @@
 <script setup></script>
 <template>
-  <h1 class="text-center text-h3 font-weight-bold">Iniciar Sesión</h1>
+  <v-card flat max-width="600" class="mx-auto my-10">
+    <v-card-title primary-title class="text-center text-h4 font-weight-bold">
+      Iniciar Sesión
+    </v-card-title>
+    <v-card-subtitle class="text-center text-h5 font-weight-bold">
+      Iniciar sesión con tu cuenta
+    </v-card-subtitle>
+    <v-form class="mt-5">
+      <v-text-field
+        type="email"
+        name="email"
+        label="Email"
+        bg-color="blue-grey-lighten-5"
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        name="password"
+        label="Password"
+        bg-color="blue-grey-lighten-5"
+      ></v-text-field>
+      <v-btn color="pink-accent-3" block> Iniciar Sesión </v-btn>
+    </v-form>
+  </v-card>
 </template>
