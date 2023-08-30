4d3d4efbf85ef79420c44f221c9fb65859cc498f
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-08-30 11:20:40 -0500

    Validacion con vee-validate

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,22 @@
-<script setup></script>
+<script setup>
+import { useForm, useField } from "vee-validate";
+import { loginSchema as validationSchema } from "../validation/loginSchema";
+
+const { handleSubmit } = useForm({ validationSchema });
+const { value: email, errorMessage: errorEmail } = useField("email");
+const { value: password, errorMessage: errorPass } = useField("password");
+
+// console.log(errorEmail);
+
+const submit = handleSubmit((values) => {
+  console.log(values);
+  if (values) {
+    console.log("Iniciando sesion");
+  } else {
+    console.log("Por favor ingresar todos los campos");
+  }
+});
+</script>
 <template>
   <v-card flat max-width="600" class="mx-auto my-10">
     <v-card-title primary-title class="text-center text-h4 font-weight-bold">
@@ -13,14 +31,19 @@
         name="email"
         label="Email"
         bg-color="blue-grey-lighten-5"
+        v-model="email"
+        :error-message="errorEmail"
       ></v-text-field>
       <v-text-field
         type="password"
         name="password"
         label="Password"
         bg-color="blue-grey-lighten-5"
+        v-model="password"
       ></v-text-field>
-      <v-btn color="pink-accent-3" block> Iniciar Sesión </v-btn>
+      <v-btn @click="submit" color="pink-accent-3" block>
+        Iniciar Sesión
+      </v-btn>
     </v-form>
   </v-card>
 </template>
